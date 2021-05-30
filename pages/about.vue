721b2356704c68c98c3df012407c79a8863e44f1
HarishRavindran22 <harish@spotknack.com>
2021-05-30 22:19:34 +0530

    feat: about page created

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,274 @@
 <template>
-  <div class="container">About</div>
+  <div class="">
+    <!-- Header -->
+    <img
+      src="~assets/images/Spotknacklogo.svg"
+      alt="Spotknack"
+      class="Group-Copy logo-company"
+    />
+    <div class="header">
+      <div class="about-content playfair-font">About Us</div>
+    </div>
+    <!-- Sub header -->
+    <div class="subheader">
+      <div class="subheader-content">
+        <p class="poppins-font">
+          Spotknack was started in June 2018 by two college friends; whose
+          vision was to help students showcase their talents to recruiters. Our
+          sole purpose is to find a perfect team player to join the companies’
+          vision who are ready to join their journey.
+        </p>
+      </div>
+    </div>
+    <!-- Journey header  -->
+    <div class="jour">
+      <div class="jour-content playfair-font">Our Journey</div>
+    </div>
+    <!-- Journey milestone -->
+    <div>
+      <div class="journey">
+        <div class="timeline">
+          <div id="about-empty-containter" class="container left">
+            <div class="content"></div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>June 10 2016</h2>
+              <p>
+                Harsha and Narendran, during their college days, initiated the
+                idea of doing a real project for good experience and exposure on
+                project management and development under the team name “Blue
+                Moon”. They started their work by automating the college
+                functions and symposiums with software’s, Applications and Web
+                Applications. For an year, they worked on external projects and
+                team Management for better experience and knowledge.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>July 26 2017</h2>
+              <p>
+                The idea of starting a company was framed and official team
+                members were formed. Harsha withdrew from College Placement in
+                order to start the company. Initially, they operated as a
+                service-based company.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>September 5 2017</h2>
+              <p>
+                The company was incorporated as “iBevy Forte Private Limited” on
+                this day. They slowly started focusing towards 2 product idea
+                development while operating in the available space inside their
+                college.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>September 22 2017</h2>
+              <p>
+                Launched our Internship program for Pre-final Years to make them
+                work real time projects.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>November 21 2017</h2>
+              <p>
+                The product idea of Skill Development by Gamification was
+                approved by Start-up India. Thus, began our truce as an approved
+                startup.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>December 1 2017</h2>
+              <p>
+                The product idea was awarded with Student Innovative idea of
+                2017 by Entrepreneurship Development Institute, Tamil Nadu.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>March 4 2018</h2>
+              <p>
+                Another product idea of inter college social network was
+                launched to understand the market. Though the colleges were
+                impressed by the idea, they could not set a strong foothold in
+                Market.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>May 19 2018</h2>
+              <p>
+                Skill Assessment product idea, took a full swing work with
+                learning from the other product idea.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>June 1 2018</h2>
+              <p>
+                iBevy , became the Official member of Forge Accelerator and
+                moved into it’ s Co -Working Space.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>June 8 2018</h2>
+              <p>
+                Started an online portal to help and understand the student’s
+                preparation towards career, initially as “Educe” in two
+                colleges.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>June 16 2018</h2>
+              <p>
+                Educe expanded across 20 + colleges with 200 + participants.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>July 26 2018</h2>
+              <p>
+                We launched our Dream Product of Student Skill Assessment
+                through Gamification.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>September 14 2018</h2>
+              <p>Our platform crossed 1200 users from 30 colleges.</p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>December 2018</h2>
+              <p>Our platform crossed 1700 users from 40 + colleges.</p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>March 2019</h2>
+              <p>Crossed 2000 + users.</p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>April 5 2019</h2>
+              <p>Facilio became our first company customer.</p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>June 1 2019</h2>
+              <p>iBevy rebranding decision was taken</p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>June 28 2019</h2>
+              <p>
+                First placement Drive was organized for Facilio at Sri Krishna
+                College of Engineering and Technology.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>July 3 2019</h2>
+              <p>iBevy was rebranded to Spotknack</p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>August 14 2019</h2>
+              <p>
+                We had breakthrough in conducting a placement process through
+                our platform at two different locations (VIT – Chennai and
+                Bangalore) accessing over 1000 + concurrently.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>Oct 1 2019</h2>
+              <p>First pay cheque from Facilio</p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>January 1 2020</h2>
+              <p>Launched the student Mock Interview Program.</p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>January 25 2020</h2>
+              <p>
+                Spotknack made it to SaaS Boomi along with 500 Companies from
+                APAC
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>August 2020</h2>
+              <p>
+                Mentoring Program was initiated to introduce earning
+                Opportunities to students and also help them prepare for career
+                in an unconventional ways.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>January 1 2021</h2>
+              <p>
+                Spotknack's Unconventional was launched to bring out the journey
+                people who made it unconventionally to their career.
+              </p>
+            </div>
+          </div>
+          <div class="container right">
+            <div class="content">
+              <h2>January 20 2021</h2>
+              <p>
+                Launch of Spotknack's Mission 2021, to create 300 employable
+                student talents and to introduce earning Opportunities to
+                students.
+              </p>
+            </div>
+          </div>
+          <div class="container left">
+            <div class="content">
+              <h2>March 22 2021</h2>
+              <p>
+                Spotknack Mission 2021 was Featured On Indian Expression Edex
+                Edition
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,34 +276,210 @@ export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.logo-company {
+  margin: auto;
+  margin-top: 210px;
+  display: block;
+}
+.header {
+  width: 100%;
   display: flex;
   justify-content: center;
-  align-items: center;
+  text-align: center;
+  margin: 0 !important;
+}
+.container {
+  padding: 10px 40px;
+  position: relative;
+  background-color: inherit;
+  width: 50%;
+}
+.about-content {
+  padding-top: 0.7%;
+  letter-spacing: 2px;
+  font-size: 60px;
+  font-weight: bold;
+}
+
+.subheader {
+  display: flex;
+  justify-content: center;
+  padding-top: 1.1%;
+}
+.subheader-content {
+  text-align: center;
+  width: 50%;
+  font-size: 1rem;
+  letter-spacing: 0.6px;
+  line-height: 1.75rem;
+}
+
+.jour {
+  width: 100%;
+  display: flex;
+  justify-content: center;
   text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.jour-content {
+  font-size: 3.75rem;
+  letter-spacing: 2px;
+  font-weight: bold;
+}
+.journey {
+  display: flex;
+  position: relative;
+  margin-top: 2.9%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.timeline {
+  position: relative;
+  margin: 0 auto;
 }
 
-.links {
-  padding-top: 15px;
+.timeline::after {
+  content: '';
+  position: absolute;
+  width: 3px;
+  background-color: rgba(0, 0, 0, 0.2);
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  margin-left: -3px;
+}
+.container::after {
+  content: '';
+  position: absolute;
+  right: -6px;
+  top: 50%;
+  border-radius: 50%;
+  z-index: 1;
+  width: 15px;
+  height: 15px;
+  background-color: #c9fd6a;
+}
+
+.left {
+  left: 0;
+}
+
+.right {
+  left: 50%;
+}
+
+.right::after {
+  left: -9px;
+}
+
+.content {
+  padding: 20px 30px;
+  background-color: white;
+  position: relative;
+  border-radius: 25px;
+  border: solid 1px rgba(0, 0, 0, 0.2);
+}
+.content h2 {
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 0.93px;
+  color: #000;
+}
+.content p {
+  font-size: 12px;
+  line-height: 1.75;
+  letter-spacing: 0.6px;
+  color: #000;
+}
+#about-empty-containter {
+  background: white !important;
+  border: white !important;
+  margin-top: -2%;
+}
+#about-empty-containter .content {
+  border: none !important;
+}
+
+@media only screen and (min-width: 991px) {
+  .footer-line {
+    width: 100%;
+  }
+  .journey {
+    width: 100%;
+  }
+
+  .timeline {
+    width: 100%;
+    margin: 0%;
+  }
+  .container {
+    margin: 0%;
+    padding: 0;
+    margin-bottom: 6%;
+  }
+  .left {
+    margin-left: 12.3%;
+    width: 24.9%;
+    left: 0;
+  }
+  .right {
+    left: 0;
+    margin-left: 62.8%;
+    width: 24.9%;
+  }
+  .right::after {
+    left: -53.3%;
+  }
+  .container::after {
+    right: -53.3%;
+  }
+  .timeline::after {
+    left: 50%;
+  }
+}
+@media only screen and (min-width: 640px) and (max-width: 991px) {
+  #about-empty-containter {
+    margin-top: -4% !important;
+  }
+  .container {
+    margin: 0 !important;
+  }
+  .about-content,
+  .jour-content {
+    font-size: 45px;
+  }
+}
+@media only screen and (min-width: 320px) and (max-width: 640px) {
+  .timeline::after {
+    left: 31px;
+  }
+
+  .container {
+    width: 100%;
+    padding-left: 70px;
+    padding-right: 25px;
+    margin: 0 !important;
+  }
+
+  .left::after,
+  .right::after {
+    left: 22px;
+  }
+
+  .right {
+    left: 0%;
+  }
+  .box {
+    width: auto;
+  }
+  .guide {
+    width: auto;
+  }
+  .about-content,
+  .jour-content {
+    font-size: 30px;
+    padding-top: 5%;
+  }
+  #about-empty-containter {
+    margin-top: -9% !important;
+  }
 }
 </style>
